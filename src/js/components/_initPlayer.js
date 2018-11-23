9787c3772927bcd105b4caa19de760a0923fbc0f
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-11-23 18:58:28 +0200

    first page

---
+++ src/js/components/_initPlayer.js
@@ -0,0 +1,6 @@
+// import { Player } from 'bitmovin-player';
+// import { UIFactory } from 'bitmovin-player';
+// // require('bitmovin-player/bitmovinplayer-ui.css');
+// var bitmovinplayer = require('bitmovin-player');
+
+
