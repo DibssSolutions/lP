9ae28d6d240ae268190448d2ab3caae9d93e5b1d
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-12-04 18:35:25 +0200

    v2.2

---
+++ src/js/components/_tags-toggle.js
@@ -0,0 +1,37 @@
+import { ACTIVE } from '../constants';
+
+const tags = document.querySelectorAll('.js-tag');
+const tagsContainer = document.querySelector('.js-tags-container');
+
+const getDirection = function(el, e) {
+  let mousePos = e.clientX;
+  let xPos = el.offsetLeft;
+  let direction;
+  direction = mousePos - xPos > 100 ? 'right' : 'left';
+  return direction;
+};
+
+if (tags) {
+  const tagsArr = [].slice.call(tags);
+  tagsArr.forEach(tag => {
+    tag.addEventListener('mouseenter', e => {
+      tag.classList.remove('to-left', 'to-right');
+      let dir = getDirection(tag, e);
+      tag.classList.add(`from-${dir}`);
+    });
+    tag.addEventListener('mouseleave', e => {
+      tag.classList.remove('from-left', 'from-right');
+      let dir = getDirection(tag, e);
+      tag.classList.add(`to-${dir}`);
+    });
+  });
+
+  tagsContainer.addEventListener('mouseenter', e => {
+    if (e.target.classList.contains(ACTIVE)) return;
+    setTimeout(() => tagsContainer.classList.add('is-hoverable'), 200);
+  });
+  tagsContainer.addEventListener('mouseleave', e => {
+    if (e.target.classList.contains(ACTIVE)) return;
+    setTimeout(() => tagsContainer.classList.remove('is-hoverable'), 200);
+  });
+}
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,3 +1,4 @@
 import './_mobMenu';
 // import './_tickets-toggle';
 import './_likes';
+import './_tags-toggle';
