2ece5eb638475fa6a25c558d23714ef09fc29416
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-12-06 13:30:07 +0200

    console.log messages deleted

--- src/js/components/_likes.js
+++ src/js/components/_likes.js
@@ -1,10 +1,8 @@
 const likes = document.querySelectorAll('.js-like');
 const likesArr = [].slice.call(likes);
-console.log(likes, likesArr);
 
 likesArr.forEach(like => {
   like.addEventListener('click', e => {
     like.classList.add('is-liked');
-    console.log('liked');
   });
 });
